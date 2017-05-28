040c0dd65c881b5f8cd8b75ce874133653dac8bf
Caishun Chen <cschen1205@gmail.com>
2017-05-29 05:12:05 +0800

    project setup

---
+++ src/jsspline.js
@@ -0,0 +1,14 @@
+var jsspline = jsspline || {};
+
+(function(jss){
+	var BSpline = function(){
+        
+    };
+
+    jss.BSpline = BSpline;
+
+})(jsspline);
+
+if(module) {
+	module.exports = jsspline;
+}
